9b55e0eb6cc366e9481db046bb8c0435b5b9b665
Daniel Andres Colina <danielcolicuar@gmail.com>
2018-03-05 22:02:48 -0800

    fix geocoding portion

--- geoCoding.R
+++ geoCoding.R
@@ -1,9 +1,19 @@
 #load ggmap
+
+#INSTALL THESE PACKAGES
+#install.packages('devtools')
+#devtools::install_github("dkahle/ggmap")
 library("ggplot2")
 library("ggmap")
+source('../apikey/apikey.R')
+
+#register using your api key
+register_google(api.key)
  
 # data frame resulting from tabledata.R program
 dat <- dat 
+
+
 
 # Loop through the addresses to get the latitude and longitude of each address and add it to the
 # 'dat' data frame in new columns lat and lon
@@ -17,7 +27,7 @@ for(i in 1:nrow(dat)) {
     dat$lon[i] <- NA
     dat$lat[i] <- NA
   }
-  Sys.sleep(0.5) # to prevent exceeding query limit time out 
+  Sys.sleep(0.3)# to prevent exceeding query limit time out 
 }
 
 
