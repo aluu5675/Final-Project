a8a86a20b8c315ed1354d704e84aa75121e09a51
Beichen Liang <liangbeichen@hotmail.com>
2018-03-06 14:51:32 -0800

    Adds ratio data for year 1970 to 2016

--- Final Project.R
+++ Final Project.R
@@ -3,16 +3,19 @@ library(dplyr)
 library(stringr)
  
 source("tabledata.R")
+source("totalpassenger.R")
 
-dat$location <- gsub("near", "", dat$location)
+dat$location <- gsub("near ", "", dat$location)
 dat <- mutate(dat, occurence = 1) 
 
 data_year <- dat
 data_year$year <- str_sub(data_year$date, -4, -1)
 
+data_year <- left_join(data_year, passenger.info, by = c("year" = "year"))
+
 data_year_general <- group_by(data_year, year) %>%
   summarize(total_occurence = sum(occurence),
-            total_fatality = sum(as.numeric(!is.na(fat.))),
+            total_fatality = sum(as.numeric(fat.), na.rm = T),
             average_fatality = mean(round(total_fatality / total_occurence, digits = 2)))
 
 data_year_military <- filter(data_year, str_detect(operator, fixed("AF")) |
@@ -22,12 +25,12 @@ data_year_military <- filter(data_year, str_detect(operator, fixed("AF")) |
 
 summary_year_military_operator <- group_by(data_year_military, operator) %>%
   summarize(total_occurence = sum(occurence),
-            total_fatality = sum(as.numeric(!is.na(fat.))),
+            total_fatality = sum(as.numeric(fat.), na.rm = T),
             average_fatality = mean(round(total_fatality / total_occurence, digits = 2)))
 
 summary_year_military_plane <- group_by(data_year_military, type) %>%
   summarize(total_occurence = sum(occurence),
-            total_fatality = sum(as.numeric(!is.na(fat.))),
+            total_fatality = sum(as.numeric(fat.), na.rm = T),
             average_fatality = mean(round(total_fatality / total_occurence, digits = 2)))
 
 
@@ -37,28 +40,36 @@ data_year_private <- filter(data_year, operator == "private" |
 
 summary_year_private_operator <- group_by(data_year_private, operator) %>%
   summarize(total_occurence = sum(occurence),
-            total_fatality = sum(as.numeric(!is.na(fat.))),
+            total_fatality = sum(as.numeric(fat.), na.rm = T),
             average_fatality = mean(round(total_fatality / total_occurence, digits = 2)))
 
 summary_year_private_plane <- group_by(data_year_private, type) %>%
   summarize(total_occurence = sum(occurence),
-            total_fatality = sum(as.numeric(!is.na(fat.))),
+            total_fatality = sum(as.numeric(fat.), na.rm = T),
             average_fatality = mean(round(total_fatality / total_occurence, digits = 2)))
 
 
 data_year_ex_military <- setdiff(data_year, data_year_military)
 data_year_other <- setdiff(data_year_ex_military, data_year_private)
 
+data_year_other_ratio <- group_by(data_year_other, year) %>%
+  summarize(total_occurence = sum(occurence),
+            total_fatality = sum(as.numeric(fat.), na.rm = T),
+            average_fatality = mean(round(total_fatality / total_occurence, digits = 2)),
+            ratio = 100 * total_fatality / passenger[1])
+
 summary_year_other <- group_by(data_year_other, operator) %>%
   summarize(total_occurence = sum(occurence),
-            total_fatality = sum(as.numeric(!is.na(fat.))),
+            total_fatality = sum(as.numeric(fat.), na.rm = T),
             average_fatality = mean(round(total_fatality / total_occurence, digits = 2)))
 
 summary_year_other_plane <- group_by(data_year_other, type) %>%
   summarize(total_occurence = sum(occurence),
-            total_fatality = sum(as.numeric(!is.na(fat.))),
+            total_fatality = sum(as.numeric(fat.), na.rm = T),
             average_fatality = mean(round(total_fatality / total_occurence, digits = 2)))
 
+
+View(data_year_other_ratio)
 
 # data_month <- dat
 # data_month$date <- str_sub(data_month$date, 4, 6)
